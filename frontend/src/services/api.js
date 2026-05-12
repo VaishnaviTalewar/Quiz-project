e13514fd81b6076408b9370d3572d641f651afa4
VaishnaviTalewar <GitHub-profile-wala-email@gmail.com>
2026-05-12 19:52:53 +0530

    frontend fixed

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,34 +1,45 @@
 import axios from "axios";
-import {useAuth} from "@clerk/react";
+import { useAuth } from "@clerk/react";
 
- const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
- //axios instance
- const apiClient = axios.create({
-    baseURL: BASE_URL,
-    headers:{
-        "Content-Type":"application/json"
+// axios instance
+const apiClient = axios.create({
+  baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
+});
+
+export const useApi = () => {
+  const { getToken } = useAuth();
+
+  const request = async (endpoint, method = "GET", body = null) => {
+    const token = await getToken();
+
+    const config = {
+      url: endpoint,
+      method: method.toUpperCase(),
+      data: body,
+      headers: token
+        ? { Authorization: `Bearer ${token}` }
+        : {},
+    };
+
+    try {
+      const response = await apiClient(config);
+      return response.data;
+    } catch (error) {
+      console.error(
+        "AXIOS ERROR:",
+        error.response?.data || error.message
+      );
+
+      throw new Error(
+        error.response?.data?.message || "API ERROR"
+      );
     }
- })
+  };
 
- export const useApi = () =>{
-    const {getToken} = useAuth();
-    const request = async (endpoint, method="GET",body=null)=>{
-        const token = await getToken();
-
-        const config = {
-            url: endpoint,
-            method: method.toUpperCase(),
-            data: body,
-            headers: token ? {Authorization:`Bearer ${token}` } : {}
-        }
-        try {
-            const response = await apiClient(config)
-            return response.data
-        } catch (error) {
-            console.error("AIXOS ERROR:",error.response?.data || error.message);
-            throw new Error(error.response?.data?.message || "API ERROR")
-        }
-    }
-    return {request}
- }
+  return { request };
+};
