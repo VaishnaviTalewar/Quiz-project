c281d3f80335aeac3459579a5c2a4cf53af75655
VaishnaviTalewar <GitHub-profile-wala-email@gmail.com>
2026-05-12 20:00:49 +0530

    final

--- admin/src/services/api.js
+++ admin/src/services/api.js
@@ -1,41 +1,57 @@
 import { useAuth } from "@clerk/clerk-react";
 
-const BASE_URL = "http://localhost:8080/api";
+const BASE_URL = import.meta.env.VITE_BACKEND_URL;
 
 export const apiRequest = async (
-    endpoint,
-    method = "GET",
-    body = null,
-    token = null,
+  endpoint,
+  method = "GET",
+  body = null,
+  token = null,
 ) => {
-    const options = {
-        method,
-        headers: {
-            "Content-Type": "application/json"
-        }
-    }
-    //attach token
-    if (token) {
-        options.headers.Authorization = `Bearer ${token}`
-    }
-    if (body) {
-        options.body = JSON.stringify(body)
-    }
-    const res = await fetch(`${BASE_URL}${endpoint}`,options);
-    if(!res.ok){
-        const errorData = await res.json().catch(()=>({}));
-        throw new Error(errorData.message || "API ERROR")
-    }
-    return res.json();
+
+  const options = {
+    method,
+    headers: {
+      "Content-Type": "application/json",
+    },
+  };
+
+  // attach token
+  if (token) {
+    options.headers.Authorization = `Bearer ${token}`;
+  }
+
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
+
+  const res = await fetch(`${BASE_URL}${endpoint}`, options);
+
+  if (!res.ok) {
+    const errorData = await res.json().catch(() => ({}));
+
+    throw new Error(errorData.message || "API ERROR");
+  }
+
+  return res.json();
 };
 
-//useApi
+// useApi
 
-export const useApi = () =>{
-    const {getToken} = useAuth()
-    const request = async (endpoint,method="GET",body = null) =>{
-        const token = await getToken();
-        return apiRequest(endpoint,method,body,token)
-    }
-    return {request};
-}
+export const useApi = () => {
+
+  const { getToken } = useAuth();
+
+  const request = async (
+    endpoint,
+    method = "GET",
+    body = null
+  ) => {
+
+    const token = await getToken();
+
+    return apiRequest(endpoint, method, body, token);
+  };
+
+  return { request };
+};
